f6f59ef3bfa9a088c6de8d7a23e8c91aabea960b
Yuya Otsubo <yuotsubo@c2r6s5.42tokyo.jp>
2024-05-08 21:52:33 +0900

    add: compress.c

---
+++ src/compress.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compress.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 21:38:48 by yuotsubo          #+#    #+#             */
+/*   Updated: 2024/05/08 21:41:23 by yuotsubo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft.h"
+
+int	*compress(int *input, int size)
+{
+	int	*res;
+
+	res = (int *)malloc(sizeof(int) * size);
+	if (!res)
+		return (NULL);
+	
+}
