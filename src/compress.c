cfadfb065f3cda7645ec9cd13e2582daca005e8c
yotsubo <513047.otsubo@gmail.com>
2024-05-14 13:45:05 +0900

    done: compress.c

--- src/compress.c
+++ src/compress.c
@@ -6,7 +6,7 @@
 /*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 21:38:48 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/05/14 13:18:42 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/05/14 13:44:38 by yotsubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,45 @@
 int	*compress(int *input, int size)
 {
 	int	*res;
-	int	prev_min;
-	int	min;
+	long long	prev_min;
+	int	min_index;
+	long long	min;
+	int	i;
+	int	j;
 
 	res = (int *)malloc(sizeof(int) * size);
 	if (!res)
 		return (NULL);
-	prev_min = INT_MIN;
-	min = INT_MAX;
+	prev_min = LLONG_MIN;
+	i = 0;
+	while (i < size)
+	{
+		min = LLONG_MAX;
+		j = 0;
+		while (j < size)
+		{
+			if ((long long)input[j] > prev_min && (long long)input[j] <= min)
+			{
+				min_index = j;
+				min = input[min_index];
+			}
+			j++;
+		}
+		res[min_index] = i++;
+		prev_min = input[min_index];
+	}
+	return (res);
 }
+
+// int	main(void)
+// {
+// 	int	input[] = {123, INT_MAX, INT_MIN, -3};
+// 	int	size = 4;
+// 	int	*res;
+
+// 	res = compress(input, size);
+// 	for (int i = 0; i < size; i++)
+// 		printf("%d\n", res[i]);
+// 	free(res);
+// 	return (0);
+// }
