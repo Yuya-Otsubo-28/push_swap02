acc00816fd5e348a61669fd0bd3ae2ffcf933b8e
Yuya Otsubo <yuotsubo@c2r2s7.42tokyo.jp>
2024-05-22 15:01:08 +0900

    add: libft/map_and_free.c

--- libft/libft.h
+++ libft/libft.h
@@ -115,4 +115,6 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+char	*map_and_free(char *res, char *for_free);
+
 #endif
